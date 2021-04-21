3581e97e849538562d42b9f2271e7f5468c955b1
Tarun Shrivastava <20je1022@ee.iitism.ac.in>
2021-04-22 01:03:59 +0530

    Errors resolved after merging from branch Tarun and Bhavya

--- app/src/main/java/app/tarsun/topchat/IntroPage.java
+++ app/src/main/java/app/tarsun/topchat/IntroPage.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class register_Activity extends AppCompatActivity {
+public class IntroPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register_);
+        setContentView(R.layout.activity_intropage);
     }
 }
--- app/src/main/java/app/tarsun/topchat/Register_Activity.java
+++ app/src/main/java/app/tarsun/topchat/Register_Activity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class intropage extends AppCompatActivity {
+public class Register_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intropage);
+        setContentView(R.layout.activity_register_);
     }
 }
